69830421a351abab689d7bb1a534fff1da4f1970
Dapros <sergiodrp70@gmail.com>
2025-09-11 12:59:29 -0500

    Feat: organizacion de la pagina

--- src/components/forms/OrderForm.tsx
+++ src/components/forms/OrderForm.tsx
@@ -1,9 +1,29 @@
+import { useOrderStore } from "../../store/useOrderStore"
 
 
 export default function OrderForm() {
+
+  // estados globales
+  const { isEditing } = useOrderStore()
+
   return (
     <div>
-      
+      <form>
+        <h3 className="text-lg font-semibold text-gray-700 mb-4">
+          { isEditing ? "Editar pedido" : "Registrar pedido"}
+        </h3>
+
+        <div className="flex flex-col space-y-2">
+          <label htmlFor="towerOrder">Elige la torre</label>
+          <select id="towerOrder">
+
+          </select>
+        </div>
+
+        <div>
+          
+        </div>
+      </form>
     </div>
   )
 }
